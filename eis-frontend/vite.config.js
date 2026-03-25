7a05c48ed13c740338c5be99ebe1e6fad74e48c1
visal2002 <115523620+visal2002@users.noreply.github.com>
2026-03-26 02:45:27 +0600

    feat: Initialize React frontend project with Vite configuration and dependencies.

--- eis-frontend/vite.config.js
+++ eis-frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [],
-      },
-    }),
+    react(),
   ],
   server: {
     host: "0.0.0.0",
